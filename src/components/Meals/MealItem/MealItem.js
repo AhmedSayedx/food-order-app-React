dc71dfa82103655366cadabb5c2150990205d6e7
Ducex <ammm12@live.com>
2023-02-03 22:10:53 +0200

    adding meals item

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  //toFixed is to show only the numbers we want to show
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
