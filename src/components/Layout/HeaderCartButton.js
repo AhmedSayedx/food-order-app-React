bcc0b4a4f4a6a02fa5086f344592702103fe8b71
Ducex <ammm12@live.com>
2023-02-06 17:32:21 +0200

    contain useReduser

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../Store/Cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCTX = useContext(CartContext);
+  const numberOfCartItems = cartCTX.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onShow}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
